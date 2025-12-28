6e2b435cdb2ed0802a05886846d0166d25d84466
EpicVon2468 <thomaschad0909@gmail.com>
2025-12-28 22:02:55 +1100

    Refactor screen

--- core/src/main/kotlin/io/github/epicvon2468/eva_cleaning_simulator/Main.kt
+++ core/src/main/kotlin/io/github/epicvon2468/eva_cleaning_simulator/Main.kt
@@ -1,16 +1,10 @@
 package io.github.epicvon2468.eva_cleaning_simulator
 
-import com.badlogic.gdx.graphics.Texture
-import com.badlogic.gdx.graphics.Texture.TextureFilter.Linear
-import com.badlogic.gdx.graphics.g2d.SpriteBatch
+import io.github.epicvon2468.eva_cleaning_simulator.screen.FirstScreen
 
 import ktx.app.KtxGame
 import ktx.app.KtxScreen
-import ktx.app.clearScreen
-import ktx.assets.disposeSafely
-import ktx.assets.toInternalFile
 import ktx.async.KtxAsync
-import ktx.graphics.use
 
 class Main : KtxGame<KtxScreen>() {
 
@@ -19,23 +13,5 @@ class Main : KtxGame<KtxScreen>() {
 
 		addScreen(FirstScreen())
 		setScreen<FirstScreen>()
-	}
-}
-
-class FirstScreen : KtxScreen {
-
-	private val image = Texture("logo.png".toInternalFile(), true).apply { setFilter(Linear, Linear) }
-	private val batch = SpriteBatch()
-
-	override fun render(delta: Float) {
-		clearScreen(red = 0.7f, green = 0.7f, blue = 0.7f)
-		batch.use {
-			it.draw(image, 100f, 160f)
-		}
-	}
-
-	override fun dispose() {
-		image.disposeSafely()
-		batch.disposeSafely()
 	}
 }
