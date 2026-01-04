09de7a5b218104679a1b6a62332634225b1bbba3
EpicVon2468 <thomaschad0909@gmail.com>
2026-01-04 13:43:34 +1100

    DoNotManuallyDispose

--- core/src/main/kotlin/io/github/epicvon2468/eva_cleaning_simulator/assets/Fonts.kt
+++ core/src/main/kotlin/io/github/epicvon2468/eva_cleaning_simulator/assets/Fonts.kt
@@ -5,6 +5,8 @@ import com.badlogic.gdx.graphics.g2d.BitmapFont
 import com.badlogic.gdx.graphics.g2d.TextureRegion
 import com.badlogic.gdx.utils.Disposable
 
+import io.github.epicvon2468.eva_cleaning_simulator.utils.DoNotManuallyDispose
+
 import ktx.assets.Asset
 import ktx.assets.getValue
 import ktx.freetype.loadFreeTypeFont
@@ -19,6 +21,7 @@ data object Fonts : Disposable {
 		minFilter = Texture.TextureFilter.Linear
 		magFilter = Texture.TextureFilter.Linear
 	}
+	@DoNotManuallyDispose(DoNotManuallyDispose.Reason.MANAGED_RESOURCE)
 	val jetBrainsMono: BitmapFont by _jetBrainsMono
 
 	@JvmField
--- core/src/main/kotlin/io/github/epicvon2468/eva_cleaning_simulator/assets/Skins.kt
+++ core/src/main/kotlin/io/github/epicvon2468/eva_cleaning_simulator/assets/Skins.kt
@@ -3,6 +3,8 @@ package io.github.epicvon2468.eva_cleaning_simulator.assets
 import com.badlogic.gdx.graphics.g2d.BitmapFont
 import com.badlogic.gdx.scenes.scene2d.ui.Skin
 
+import io.github.epicvon2468.eva_cleaning_simulator.utils.DoNotManuallyDispose
+
 import ktx.style.label
 import ktx.style.skin
 
@@ -19,6 +21,7 @@ data object Skins {
 
 	@JvmField
 	@Suppress("GDXKotlinStaticResource") // Skin class tries to dispose all objects contained within, could have unwanted effects
+	@DoNotManuallyDispose(DoNotManuallyDispose.Reason.UNINTENDED_BEHAVIOUR)
 	val preResourceLoad: Skin = skin {
 		add("default", Fonts.preResourceLoad, BitmapFont::class.java)
 		label {
