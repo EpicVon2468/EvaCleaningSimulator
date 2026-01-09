9e7c909e54345a97f5090da22742d4b93664c587
EpicVon2468 <thomaschad0909@gmail.com>
2026-01-09 19:51:41 +1100

    Gradle formatting, prepare to fully move out stuff to gdx_helpers (prepend with typalias)

--- core/src/main/kotlin/io/github/epicvon2468/eva_cleaning_simulator/screens/world/TutorialWorldScreen.kt
+++ core/src/main/kotlin/io/github/epicvon2468/eva_cleaning_simulator/screens/world/TutorialWorldScreen.kt
@@ -2,10 +2,11 @@ package io.github.epicvon2468.eva_cleaning_simulator.screens.world
 
 import io.github.epicvon2468.eva_cleaning_simulator.Main
 import io.github.epicvon2468.eva_cleaning_simulator.actors.SpriteActor
+import io.github.epicvon2468.eva_cleaning_simulator.assets.Textures
 
 class TutorialWorldScreen(main: Main) : WorldScreen(main) {
 
 	override fun postInit() {
-		stage.addActor(SpriteActor().debug())
+		stage.addActor(SpriteActor(Textures.notFoundTexture).debug())
 	}
 }
