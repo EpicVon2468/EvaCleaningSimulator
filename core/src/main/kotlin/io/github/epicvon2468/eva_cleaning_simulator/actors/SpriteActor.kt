79682fdc060f66f89a7245b89b326b923c294209
EpicVon2468 <thomaschad0909@gmail.com>
2026-01-09 16:59:26 +1100

    SpriteActor++

--- core/src/main/kotlin/io/github/epicvon2468/eva_cleaning_simulator/actors/SpriteActor.kt
+++ core/src/main/kotlin/io/github/epicvon2468/eva_cleaning_simulator/actors/SpriteActor.kt
@@ -4,37 +4,51 @@ import com.badlogic.gdx.graphics.Texture
 import com.badlogic.gdx.graphics.g2d.Batch
 import com.badlogic.gdx.graphics.g2d.Sprite
 import com.badlogic.gdx.scenes.scene2d.Actor
-import com.badlogic.gdx.scenes.scene2d.Stage
 
 import io.github.epicvon2468.eva_cleaning_simulator.assets.Textures
-import io.github.epicvon2468.eva_cleaning_simulator.utils.centrePosition
+
+import org.jetbrains.annotations.ApiStatus
 
 interface SpriteRenderable {
 
+	@get:ApiStatus.Internal
 	val sprite: Sprite
 
 	fun draw(batch: Batch, parentAlpha: Float) = this.sprite.draw(batch, parentAlpha)
 }
 
 open class SpriteActor(
-	override val sprite: Sprite = Sprite(Textures.notFoundTexture),
-	x: Float = 0f,
-	y: Float = 0f
+	@ApiStatus.Internal
+	override val sprite: Sprite = Sprite(Textures.notFoundTexture)
 ) : Actor(), SpriteRenderable {
 
-	override fun setStage(stage: Stage?) {
-		super.setStage(stage)
-		if (stage != null) this.centrePosition()
-	}
+	constructor(texture: Texture) : this(Sprite(texture))
 
 	init {
-		//this.setPosition((x - sprite.width), (y + sprite.height))
-		this.setBounds(x, y, sprite.width, sprite.height)
+		this.setBounds(this.x, this.y, this.sprite.width, this.sprite.height)
+		this.setScale(this.sprite.scaleX, this.sprite.scaleY)
+		this.rotation = this.sprite.rotation
 	}
 
-	override fun positionChanged() = this.sprite.setPosition(this.x, this.y)
-
-	constructor(texture: Texture, x: Float = 0f, y: Float = 0f) : this(Sprite(texture), x, y)
+	override fun positionChanged() {
+		val x: Float = this.x
+		val y: Float = this.y
+		if (this.sprite.x != x || this.sprite.y != y) this.sprite.setPosition(x, y)
+	}
+	override fun sizeChanged() {
+		val width: Float = this.width
+		val height: Float = this.height
+		if (this.sprite.width != width || this.sprite.height != height) this.sprite.setSize(width, height)
+	}
+	override fun scaleChanged() {
+		val scaleX: Float = this.scaleX
+		val scaleY: Float = this.scaleY
+		if (this.sprite.scaleX != scaleX || this.sprite.scaleY != scaleY) this.sprite.setScale(scaleX, scaleY)
+	}
+	override fun rotationChanged() {
+		val rotation: Float = this.rotation
+		if (this.sprite.rotation != rotation) this.sprite.rotation = rotation
+	}
 
 	override fun draw(batch: Batch, parentAlpha: Float) = super<SpriteRenderable>.draw(batch, parentAlpha)
 }
--- core/src/main/kotlin/io/github/epicvon2468/eva_cleaning_simulator/screens/world/TutorialWorldScreen.kt
+++ core/src/main/kotlin/io/github/epicvon2468/eva_cleaning_simulator/screens/world/TutorialWorldScreen.kt
@@ -6,6 +6,6 @@ import io.github.epicvon2468.eva_cleaning_simulator.actors.SpriteActor
 class TutorialWorldScreen(main: Main) : WorldScreen(main) {
 
 	override fun postInit() {
-		stage.addActor(SpriteActor(x = (16f * 20) / 2, y = (12f * 20) / 2).debug())
+		stage.addActor(SpriteActor().debug())
 	}
 }
