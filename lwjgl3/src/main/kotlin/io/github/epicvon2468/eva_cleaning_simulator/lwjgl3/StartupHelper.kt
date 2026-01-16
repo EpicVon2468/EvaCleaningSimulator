dba5da0df85162ac12b604dc021a6a6998fb776f
EpicVon2468 <thomaschad0909@gmail.com>
2026-01-16 11:55:25 +1100

    Partially update StartupHelper.kt

--- lwjgl3/src/main/kotlin/io/github/epicvon2468/eva_cleaning_simulator/lwjgl3/StartupHelper.kt
+++ lwjgl3/src/main/kotlin/io/github/epicvon2468/eva_cleaning_simulator/lwjgl3/StartupHelper.kt
@@ -28,18 +28,19 @@ import com.badlogic.gdx.backends.lwjgl3.Lwjgl3NativesLoader
  */
 object StartupHelper {
 
+	// TODO: Bring in the new version of StartupHelperKt, and figure out how to make it work with FabricLoader?  Or just intercept the Fabric 'main' with an entirely separate main that calls this first...
 	fun help() {
-		if (System.getProperty("os.name").lowercase().contains("windows")) {
+		if ("windows" in System.getProperty("os.name").lowercase()) {
 			// Here, we are trying to work around an issue with how LWJGL3 loads its extracted .dll files.
 			// By default, LWJGL3 extracts to the directory specified by "java.io.tmpdir", which is usually the user's home.
 			// If the user's name has non-ASCII (or some non-alphanumeric) characters in it, that would fail.
 			// By extracting to the relevant "ProgramData" folder, which is usually "C:\ProgramData", we avoid this.
 			// We also temporarily change the "user.name" property to one without any chars that would be invalid.
 			// We revert our changes immediately after loading LWJGL3 natives.
-			val programData = System.getenv("ProgramData") ?: "C:\\Temp\\"
-			val prevTmpDir = System.getProperty("java.io.tmpdir", programData)
-			val prevUser = System.getProperty("user.name", "libGDX_User")
-			System.setProperty("java.io.tmpdir", "$programData/libGDX-temp")
+			val programData: String = System.getenv("ProgramData") ?: "C:\\Temp"
+			val prevTmpDir: String = System.getProperty("java.io.tmpdir", programData)
+			val prevUser: String = System.getProperty("user.name", "libGDX_User")
+			System.setProperty("java.io.tmpdir", "$programData\\libGDX-temp")
 			System.setProperty(
 				"user.name",
 				"User_${prevUser.hashCode()}_GDX${Version.VERSION}".replace('.', '_')
